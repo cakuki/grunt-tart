8ab1be3f6001db698a73d9077e41dd0c5f81b927
Can Kutlu Kinay <ckkinay@gmail.com>
2014-12-06 02:37:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# grunt-tart
+
+> tartJS + Cordova build plugin.
+
+## Getting Started
+This plugin requires Grunt `~0.4.5`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-tart --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-tart');
+```
+
+## The "tart" task
+
+### Overview
+In your project's Gruntfile, add a section named `tart` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+  tart: {
+    options: {
+      compilerDefs: {
+          'goog.DEBUG': false
+      },
+      cordova: {
+        id: 'com.tart.example-app',
+        name: 'Example tartJS App',
+        path: 'target'
+      }
+    },
+    ios: {
+      options: {
+        compilerDefs: {
+          'goog.userAgent.ASSUME_WEBKIT': true
+        }
+      }
+    },
+    wp8: {
+      options: {
+        compilerDefs: {
+          'goog.userAgent.ASSUME_IE': true
+        }
+      }
+    },
+  },
+});
+```
+
+Note that objects under options are overridden completely when target option with same key defined. See [grunt issue #738](https://github.com/gruntjs/grunt/issues/738).
+
+### Options
+
+#### options.compilerDefs
+Type: `Object.<String, [Boolean|String|Number]>`
+Default value: `{ 'goog.DEBUG': true }`
+
+Closure Compiler definition key pairs. Can be changed via `define` cli flag:
+
+` grunt tart:ios --define="goog.DEBUG=false,goog.LOCALE=fr" `
+
+#### options.cordova
+Type: `Object`
+Default value: `{
+                    id: 'com.tart.example-app',
+                    name: 'Example tartJS App',
+                    path: 'target'
+                }`
+
+These options passed into [grunt-cordovacli](https://github.com/csantanapr/grunt-cordovacli) task. See [grunt-cordovacli options documentation](https://github.com/csantanapr/grunt-cordovacli#options) for details.
+
+### Usage Examples
+
+(Coming soon)
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+
+* Initial release (06/12/2014 02:36)
---
+++ tasks/tart.js
@@ -0,0 +1,76 @@
+/*
+ * grunt-tart
+ * https://github.com/cakuki/grunt-tart
+ *
+ * Copyright (c) 2014 Startup Kitchen
+ * Licensed under the Apache-2.0 license.
+ */
+
+'use strict';
+
+var _ = require('lodash');
+var parseOptions = require('../lib/parseOptions');
+
+module.exports = function tart(grunt) {
+
+    grunt.registerMultiTask('tart', 'tartJS + Cordova build plugin.', function() {
+        var options = this.options({
+            compilerDefs: {
+                'goog.DEBUG': true
+            },
+            cordova: {
+                id: 'com.tart.example-app',
+                name: 'Example tartJS App',
+                path: 'target'
+            }
+        });
+        var level = grunt.option('link') ? 'dev' : 'production';
+        var platform = grunt.option('web') ? 'web' : 'device';
+        var env = grunt.option('env') || 'test';
+        var target = this.target;
+
+        var cdvArgs = [];
+
+        if (!grunt.file.isFile('config', env + '.js')) {
+            grunt.warn('Unknown environment.\n');
+        }
+
+        if (!grunt.file.isDir(options.cordova.path, 'platforms', target)) {
+            grunt.log.writeln('"' + target + '" platform not found, trying to add automatically.');
+            grunt.log.writeln(JSON.stringify(options));
+            grunt.config.set('cordovacli.options', options.cordova);
+            grunt.config.set('cordovacli.autoAddPlatform', {
+                options: {
+                    command: 'platform',
+                    action: 'add',
+                    platforms: [target]
+                }
+            });
+            grunt.task.run('cordovacli:autoAddPlatform');
+        }
+
+        if (grunt.option('no-run') && grunt.option('release')) {
+            cdvArgs.push('--release');
+            if (grunt.option('env') && grunt.option('env') !== 'production') {
+                grunt.warn('Release flag should not be used with environments other than production.');
+            }
+            env = 'production';
+            level = 'production';
+        }
+        if (grunt.option('run') === 'device') {
+            cdvArgs.push('--device');
+        }
+
+        grunt.config.set('closureBuilder.options.compilerOpts.define',
+            parseOptions(options.compilerDefs, grunt.option('define')));
+        grunt.config.set('cordovacli.options', options.cordova);
+        grunt.config.set('cordovacli.autobuild.options', {
+            command: grunt.option('run') ? 'run' : 'build',
+            platforms: [target],
+            args: cdvArgs
+        });
+
+        grunt.task.run(level, platform, env, 'cordovacli:autobuild');
+    });
+
+};
